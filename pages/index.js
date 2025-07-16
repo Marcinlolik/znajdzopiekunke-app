0fa36699e7bbd6d3d044b3fea6ddfa658733c44e
Marcinlolik <marcin_lolik@hotmail.com>
2025-07-16 22:22:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Witamy na znajdzopiekunke.pl</h1>
-      <p>Profesjonalne połączenie opiekunek z osobami potrzebującymi wsparcia.</p>
-    </div>
-  );
-}
+
